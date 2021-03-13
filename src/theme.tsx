a9ebfa05e52ffea420d9333298d3755de053e826
Joseph Vano <joseph.vano@gmail.com>
2021-03-13 11:44:19 -0500

    chore(formatting) theme
    * remove utils

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,21 +1,23 @@
 import {createMuiTheme} from "@material-ui/core";
-import {red} from "@material-ui/core/colors";
+import {red}            from "@material-ui/core/colors";
 
 const theme = createMuiTheme({
-    palette: {
-        primary: {
-            main: '#212121',
-        },
-        secondary: {
-            main: '#29b6f6',
-        },
-        error: {
-            main: red.A400,
-        },
-        background: {
-            default: '#484848',
-        },
+  palette: {
+    primary   : {
+      main: '#212121',
     },
+    secondary : {
+      main: '#29b6f6',
+    },
+    error     : {
+      main: red.A400,
+    },
+    background: {
+
+
+      default: '#484848',
+    },
+  },
 });
 
 export default theme;
